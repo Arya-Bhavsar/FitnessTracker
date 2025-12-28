84d11922685efcbdfb68273adc9393a2d0497337
Arya Bhavsar <aryab2914@gmail.com>
2025-12-28 15:51:09 -0500

    Updated file structure

--- FitnessTracker/FitnessTrackerApp.swift
+++ FitnessTracker/FitnessTrackerApp.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct FitnessTrackerApp: App {
+    // Initialize Firebase when the app starts
+    init() {
+        FirebaseApp.configure()
+        print("Firebase is configured: \(FirebaseApp.app() != nil)")
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
