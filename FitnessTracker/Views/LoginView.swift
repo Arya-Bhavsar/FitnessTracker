efa668b3c2b119343959fe685454a78ac195d32a
Arya Bhavsar <aryab2914@gmail.com>
2025-12-29 15:15:26 -0500

    Added navigation between login and signup pages

--- FitnessTracker/Views/LoginView.swift
+++ FitnessTracker/Views/LoginView.swift
@@ -21,64 +21,73 @@ struct LoginView: View {
     @FocusState private var focusedField: Field?
     
     var body: some View {
-        VStack {
-            Text("Log In")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            
-            // Email text field
-            TextField("Email", text: $email)
-                .autocapitalization(.none)
-                .focused($focusedField, equals: Field.email)
-                .fieldStyle(isFocused: focusedField == Field.email)
-                .padding(10)
-            
-            // Password field with show/hide password toggle
-            ZStack(alignment: .trailing) {
-                // Conditionally switches between text and secure fields
-                Group {
-                    if isPasswordVisible {
-                        TextField("Password", text: $password)
-                    } else {
-                        SecureField("Password", text: $password)
-                    }
-                }
-                .autocapitalization(.none)
-                .textInputAutocapitalization(.never)
-                .focused($focusedField, equals: Field.password)
-                .fieldStyle(isFocused: focusedField == Field.password)
-                .padding(10)
-                
-                // Button to toggle between show/hide password
-                Button {
-                    isPasswordVisible.toggle()
-                } label: {
-                    Image(systemName: isPasswordVisible ? "eye.slash" : "eye")
-                }
-                .padding(.trailing, 20)
-            }
-            
-            // Log in button
-            Button {
-                // Logic here
-            } label: {
+        NavigationStack {
+            VStack {
                 Text("Log In")
-                    .fontWeight(.semibold)
-                    .frame(maxWidth: .infinity)
-                    .padding(15)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(8)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                // Email text field
+                TextField("Email", text: $email)
+                    .autocapitalization(.none)
+                    .focused($focusedField, equals: Field.email)
+                    .fieldStyle(isFocused: focusedField == Field.email)
+                    .padding(10)
+                
+                // Password field with show/hide password toggle
+                ZStack(alignment: .trailing) {
+                    // Conditionally switches between text and secure fields
+                    Group {
+                        if isPasswordVisible {
+                            TextField("Password", text: $password)
+                        } else {
+                            SecureField("Password", text: $password)
+                        }
+                    }
+                    .autocapitalization(.none)
+                    .textInputAutocapitalization(.never)
+                    .focused($focusedField, equals: Field.password)
+                    .fieldStyle(isFocused: focusedField == Field.password)
+                    .padding(10)
+                    
+                    // Button to toggle between show/hide password
+                    Button {
+                        isPasswordVisible.toggle()
+                    } label: {
+                        Image(systemName: isPasswordVisible ? "eye.slash" : "eye")
+                    }
+                    .padding(.trailing, 20)
+                }
+                
+                // Log in button
+                Button {
+                    // Logic here
+                } label: {
+                    Text("Log In")
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity)
+                        .padding(15)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                .buttonStyle(.plain)
+                .padding(.horizontal, 10)
+                .padding(.top, 30)
+                
+                // Navigation to the signup page
+                HStack {
+                    Text("Don't have an account?")
+                    NavigationLink("Sign Up", destination: SignupView())
+                    Spacer()
+                }
+                .padding(.horizontal, 10)
             }
-            .buttonStyle(.plain)
-            .padding(.horizontal, 10)
-            .padding(.vertical, 30)
-            
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .contentShape(Rectangle())
+            .onTapGesture { focusedField = nil }
+            .padding()
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .contentShape(Rectangle())
-        .onTapGesture { focusedField = nil }
-        .padding()
     }
 }
 
--- FitnessTracker/Views/SignupView.swift
+++ FitnessTracker/Views/SignupView.swift
@@ -23,77 +23,87 @@ struct SignupView: View {
     @FocusState private var focusedField: Field?
     
     var body: some View {
-        VStack {
-            Text("Sign Up")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            
-            // First name text field
-            TextField("First Name", text: $firstName)
-                .focused($focusedField, equals: Field.firstName)
-                .fieldStyle(isFocused: focusedField == Field.firstName)
-                .padding(10)
-            
-            // Last name text field
-            TextField("Last Name", text: $lastName)
-                .focused($focusedField, equals: Field.lastName)
-                .fieldStyle(isFocused: focusedField == Field.lastName)
-                .padding(10)
-            
-            // Email text field
-            TextField("Email", text: $email)
-                .textContentType(.emailAddress)
-                .keyboardType(.emailAddress)
-                .autocapitalization(.none)
-                .focused($focusedField, equals: Field.email)
-                .fieldStyle(isFocused: focusedField == Field.email)
-                .padding(10)
-            
-            // Password field with show/hide password toggle
-            ZStack(alignment: .trailing) {
-                // Conditionally switches between text and secure fields
-                Group {
-                    if isPasswordVisible {
-                        TextField("Password", text: $password)
-                    } else {
-                        SecureField("Password", text: $password)
-                    }
-                }
-                .autocapitalization(.none)
-                .textInputAutocapitalization(.never)
-                .focused($focusedField, equals: Field.password)
-                .fieldStyle(isFocused: focusedField == Field.password)
-                .padding(10)
-                
-                // Button to toggle between show/hide password
-                Button {
-                    isPasswordVisible.toggle()
-                } label: {
-                    Image(systemName: isPasswordVisible ? "eye.slash" : "eye")
-                }
-                .padding(.trailing, 20)
-            }
-            
-            // Sign up button
-            Button {
-                // Logic here
-            } label: {
+        NavigationStack {
+            VStack {
                 Text("Sign Up")
-                    .fontWeight(.semibold)
-                    .frame(maxWidth: .infinity)
-                    .padding(15)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(8)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                // First name text field
+                TextField("First Name", text: $firstName)
+                    .focused($focusedField, equals: Field.firstName)
+                    .fieldStyle(isFocused: focusedField == Field.firstName)
+                    .padding(10)
+                
+                // Last name text field
+                TextField("Last Name", text: $lastName)
+                    .focused($focusedField, equals: Field.lastName)
+                    .fieldStyle(isFocused: focusedField == Field.lastName)
+                    .padding(10)
+                
+                // Email text field
+                TextField("Email", text: $email)
+                    .textContentType(.emailAddress)
+                    .keyboardType(.emailAddress)
+                    .autocapitalization(.none)
+                    .focused($focusedField, equals: Field.email)
+                    .fieldStyle(isFocused: focusedField == Field.email)
+                    .padding(10)
+                
+                // Password field with show/hide password toggle
+                ZStack(alignment: .trailing) {
+                    // Conditionally switches between text and secure fields
+                    Group {
+                        if isPasswordVisible {
+                            TextField("Password", text: $password)
+                        } else {
+                            SecureField("Password", text: $password)
+                        }
+                    }
+                    .autocapitalization(.none)
+                    .textInputAutocapitalization(.never)
+                    .focused($focusedField, equals: Field.password)
+                    .fieldStyle(isFocused: focusedField == Field.password)
+                    .padding(10)
+                    
+                    // Button to toggle between show/hide password
+                    Button {
+                        isPasswordVisible.toggle()
+                    } label: {
+                        Image(systemName: isPasswordVisible ? "eye.slash" : "eye")
+                    }
+                    .padding(.trailing, 20)
+                }
+                
+                // Sign up button
+                Button {
+                    // Logic here
+                } label: {
+                    Text("Sign Up")
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity)
+                        .padding(15)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                .buttonStyle(.plain)
+                .padding(.horizontal, 10)
+                .padding(.top, 30)
+                
+                // Navigation to the login page
+                HStack {
+                    Text("Already have an account?")
+                    NavigationLink("Log In", destination: LoginView())
+                    Spacer()
+                }
+                .padding(.horizontal, 10)
             }
-            .buttonStyle(.plain)
-            .padding(.horizontal, 10)
-            .padding(.vertical, 30)
+            .frame(maxWidth: .infinity, maxHeight: .infinity) // Needed to make empty space tappable
+            .contentShape(Rectangle()) // Needed to make empty space tappable
+            .onTapGesture { focusedField = nil }
+            .padding()
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity) // Needed to make empty space tappable
-        .contentShape(Rectangle()) // Needed to make empty space tappable
-        .onTapGesture { focusedField = nil }
-        .padding()
     }
 }
 
