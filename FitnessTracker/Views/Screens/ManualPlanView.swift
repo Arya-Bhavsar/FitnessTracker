f3589801d97dedc416ee79c788ea2362093d238d
Arya Bhavsar <aryab2914@gmail.com>
2026-01-08 18:18:17 -0500

    Added a base form for manual plan view

--- FitnessTracker/Views/Screens/ManualPlanView.swift
+++ FitnessTracker/Views/Screens/ManualPlanView.swift
@@ -8,8 +8,17 @@
 import SwiftUI
 
 struct ManualPlanView: View {
+    @State private var isPresented: Bool = false
+    
     var body: some View {
-        Text("Manual Plan View")
+        NavigationStack {
+            Form {
+                Section(header: Text("Monday")) {
+                    Toggle("Rest Day", isOn: $isPresented)
+                }
+            }
+            .navigationTitle("Weekly Plan")
+        }
     }
 }
 
