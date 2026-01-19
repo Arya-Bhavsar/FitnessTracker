32d62f3224221d4d662e296f7dcac78f5ea60e72
Arya Bhavsar <aryab2914@gmail.com>
2026-01-19 17:24:50 -0500

    Added a view for plan details

--- FitnessTracker/Views/Components/PlanRowView.swift
+++ FitnessTracker/Views/Components/PlanRowView.swift
@@ -11,32 +11,41 @@ struct PlanRowView: View {
     @EnvironmentObject var workoutPlanVM: WorkoutPlanViewModel
     
     let plan: WorkoutPlanModel
+    
     @State private var showDeletePlanAlert: Bool = false
     
     var body: some View {
-        HStack {
-            Text(plan.name)
-            
-            Spacer()
-            
-            // Menu for the plan. Options include select a plan as the current plan or delete a plan
-            Menu {
-                Button("Select Plan", systemImage: "checkmark.circle") {
-                    //
-                }
-                
-                Button("Delete Plan", systemImage: "trash", role: .destructive) {
-                    showDeletePlanAlert = true
-                }
+        NavigationStack {
+            NavigationLink {
+                PlanDetailsView(plan: plan)
             } label: {
-                Image(systemName: "ellipsis")
-                    .foregroundColor(.black)
-            }
-            .alert("Are you sure you want to delete the plan?", isPresented: $showDeletePlanAlert) {
-                // Alert to delete a plan
-                Button("Confirm", role: .destructive) {
-                    Task {
-                        await workoutPlanVM.deletePlan(plan: plan)
+                HStack {
+                    Text(plan.name)
+                    
+                    Spacer()
+                    
+                    // Menu for the plan. Options include select a plan as the current plan or delete a plan
+                    Menu {
+                        // Button to set the plan as the current plan
+                        Button("Set as Current", systemImage: "checkmark.circle") {
+                            //
+                        }
+                        
+                        // Button to delete the plan
+                        Button("Delete Plan", systemImage: "trash", role: .destructive) {
+                            showDeletePlanAlert = true
+                        }
+                    } label: {
+                        Image(systemName: "ellipsis")
+                            .foregroundColor(.black)
+                    }
+                    .alert("Are you sure you want to delete the plan?", isPresented: $showDeletePlanAlert) {
+                        // Alert to delete a plan
+                        Button("Confirm", role: .destructive) {
+                            Task {
+                                await workoutPlanVM.deletePlan(plan: plan)
+                            }
+                        }
                     }
                 }
             }
---
+++ FitnessTracker/Views/Screens/PlanDetailsView.swift
@@ -0,0 +1,16 @@
+//
+//  PlanDetailsView.swift
+//  FitnessTracker
+//
+//  Created by Arya Bhavsar on 1/19/26.
+//
+
+import SwiftUI
+
+struct PlanDetailsView: View {
+    let plan: WorkoutPlanModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
